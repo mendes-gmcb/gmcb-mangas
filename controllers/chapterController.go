070f694d7004866adf4bed42d6e72c54b39335c8
gmcb mendes <gmendescamargodebarros@gmail.com>
2023-11-18 01:45:34 -0300

    add feature to create chapter without your images

--- controllers/chapterController.go
+++ controllers/chapterController.go
@@ -2,14 +2,33 @@ package controllers
 
 import (
 	"net/http"
+	"trabalho/initializers"
+	"trabalho/models"
+	"trabalho/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 func ChapterCreate(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "pong",
-	})
+	var body models.Chapter
+	c.Bind(&body)
+
+	chapterID := utils.GenerateUUID()
+
+	chapter := models.Chapter{
+		ID:            chapterID,
+		MangaID:       body.MangaID,
+		ChapterNumber: body.ChapterNumber,
+		NumPages:      body.NumPages,
+	}
+
+	result := initializers.DB.Create(&chapter)
+	if result.Error != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"chapter": chapter})
 }
 
 func ChapterList(c *gin.Context) {
