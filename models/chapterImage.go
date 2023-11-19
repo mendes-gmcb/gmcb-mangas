0a58a0b1fe3e7f5fe4de774b58998920e5d2ed51
gmcb mendes <gmendescamargodebarros@gmail.com>
2023-11-19 10:38:43 -0300

    update chapter image model to use uuid

--- models/chapterImage.go
+++ models/chapterImage.go
@@ -1,13 +1,14 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type ChapterImage struct {
 	gorm.Model
-	ChapterID  uint
+	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
+	ChapterID  uuid.UUID
 	ImagePath  string
 	ImageOrder int
-	ImageName  string
 }
