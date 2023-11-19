6c84f42211eac966773371c02c2e0c87ba5797a8
rodolfo marques ribeiro <rodolfomarqribeiro@gmail.com>
2023-11-19 18:07:29 -0300

    refact: change database connection logic

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"log"
+	"trabalho/env"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,16 +11,29 @@ import (
 
 var DB *gorm.DB
 
-func ConectToDB() (db *gorm.DB, err error) {
-	conf := getDB()
+func Load() {
+	Connect()
+	Migrate()
+}
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
+func Connect() (*gorm.DB, error) {
+	dsn := buildDSN()
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to  connect to database")
+		log.Fatal("Failed to connect to database")
 	}
 
-	return
+	DB = db
+
+	return db, nil
+}
+
+func buildDSN() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		env.DB_CONFIG.Host,
+		env.DB_CONFIG.Port,
+		env.DB_CONFIG.User,
+		env.DB_CONFIG.Pass,
+		env.DB_CONFIG.Database,
+	)
 }
