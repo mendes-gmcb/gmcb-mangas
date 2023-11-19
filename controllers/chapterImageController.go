97f30cb13cef1532c9eaee54d1a4240759da3a39
gmcb mendes <gmendescamargodebarros@gmail.com>
2023-11-19 10:34:25 -0300

    create chapter images when create chapter

--- controllers/chapterImageController.go
+++ controllers/chapterImageController.go
@@ -1,9 +1,16 @@
 package controllers
 
 import (
+	"fmt"
+	"mime/multipart"
 	"net/http"
+	"sync"
+	"trabalho/initializers"
+	"trabalho/models"
+	"trabalho/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func ChapterImageCreate(c *gin.Context) {
@@ -39,3 +46,41 @@ func ChapterImageDelete(c *gin.Context) {
 // func upload(fileinfo os.FileInfo) {
 // 	file
 // }
+
+var wg sync.WaitGroup
+
+func ChapterImagesCreate(files []*multipart.FileHeader, mangaID, chapterID uuid.UUID, cn chan<- bool) {
+	err_cn := make(chan error)
+	semaphore := make(chan struct{}, 50)
+	var chapterImages []models.ChapterImage
+
+	for i, file := range files {
+		wg.Add(1)
+		semaphore <- struct{}{}
+
+		chapterImageID := utils.GenerateUUID()
+
+		filepath := fmt.Sprintf("/mangas/%s/%s/%s-%s", mangaID, chapterID, chapterImageID, file.Filename)
+
+		go utils.UploadMultipleImagesToS3(file, filepath, &wg, err_cn, semaphore)
+
+		if <-err_cn != nil {
+			continue
+		}
+
+		chapterImage := models.ChapterImage{
+			ID:         chapterImageID,
+			ChapterID:  chapterID,
+			ImagePath:  filepath,
+			ImageOrder: i,
+		}
+
+		chapterImages = append(chapterImages, chapterImage)
+	}
+
+	initializers.DB.Create(chapterImages)
+
+	wg.Wait()
+
+	cn <- true
+}
