95cfb56d87e32c4672edb87e9fd8c589e8458938
gmcb mendes <gmendescamargodebarros@gmail.com>
2023-11-19 17:56:22 -0300

    defining unique constraints on database

--- models/chapter.go
+++ models/chapter.go
@@ -8,8 +8,8 @@ import (
 type Chapter struct {
 	gorm.Model
 	ID            uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
-	MangaID       uuid.UUID
-	ChapterNumber int
+	MangaID       uuid.UUID `gorm:"index:idx_unique_chapter,unique,priority:1"`
+	ChapterNumber int       `gorm:"index:idx_unique_chapter,unique,priority:2"`
 	NumPages      int
 
 	Images []ChapterImage `gorm:"constraint:OnDelete:CASCADE; foreignKey:ChapterID"`
