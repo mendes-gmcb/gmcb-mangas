7beddeb9e231cc01b05542af6ed3932da28e7519
gmcb mendes <gmendescamargodebarros@gmail.com>
2023-11-20 19:33:24 -0300

    remove index for reorder images and chapters

--- models/chapterImage.go
+++ models/chapterImage.go
@@ -8,7 +8,7 @@ import (
 type ChapterImage struct {
 	gorm.Model
 	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
-	ChapterID  uuid.UUID `gorm:"index:idx_unique_page,unique,priority:1"`
-	ImagePath  string    `gorm:"index:idx_unique_image,unique"`
-	ImageOrder int       `gorm:"index:idx_unique_page,unique,priority:2"`
+	ChapterID  uuid.UUID
+	ImagePath  string
+	ImageOrder int
 }
